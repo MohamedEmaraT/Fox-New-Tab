ba8f38bd418ecefbf22e4ef11ca1599c53e05a3a
MohamedEmaraT <138072127+MohamedEmaraT@users.noreply.github.com>
2024-05-26 00:17:09 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,36 +3,33 @@ const selected = document.getElementById("selected");
 const links = document.querySelectorAll("#linkSelect a");
 const searchInput = document.getElementById("search");
 
-// Update the selected search engine based on URL hash
-const hash = window.location.hash.slice(1);
-if (hash) {
-    const selectedLink = document.querySelector(`#${hash}`);
-    if (selectedLink) {
-        selected.innerText = selectedLink.innerText;
-        searchInput.placeholder = `search - ${selected.innerText}`;
-    }
-}
-
-// Update the URL hash when an <a> tag is clicked
 links.forEach(link => {
     link.addEventListener("click", function (event) {
-        event.preventDefault(); // prevent the link from navigating
-        selected.innerText = link.innerText;
-        searchInput.placeholder = `search - ${selected.innerText}`;
-        window.location.hash = link.id;
+        event.preventDefault();
+        selected.innerText = link.innerText
     });
 });
 
-// Open the search engine with the user's search query when the form is submitted
-function openLink(event) {
-    event.preventDefault(); // prevent the form from submitting
-    const selectedLink = document.querySelector(`#search_${selected.innerText}`);
-    const query = searchInput.value;
-    const searchUrl = selectedLink.href + encodeURIComponent(query);
-    window.open(searchUrl, "_blank");
-}
+const searchButton = document.querySelector("[type=submit]").addEventListener("click", function (event) {
+    switch (selected.innerText) {
+        case 'Google':
+            window.open(`https://www.google.com/search?q=${encodeURIComponent(searchInput.value)}`, "_blank");
+            break;
+        case 'Bing':
+            window.open(`https://www.bing.com/search?q=${encodeURIComponent(searchInput.value)}`, "_blank");
+            break;
+        case 'Yahoo':
+            window.open(`https://search.yahoo.com/search?p=${encodeURIComponent(searchInput.value)}`, "_blank");
+            break;
+        case 'Ask':
+            window.open(`https://www.ask.com/web?q=${encodeURIComponent(searchInput.value)}`, "_blank");
+            break;
+        default:
+            window.open(`https://cse.google.com/cse?cx=01cc40cfca13b4128#q=lord&gsc.tab=0&gsc.q=${encodeURIComponent(searchInput.value)}&gsc.sort=`, "_blank");
+            break;
+    }
+});
 
-// Attach the form submission handler
 const form = document.querySelector("form");
 form.addEventListener("submit", openLink);
 
